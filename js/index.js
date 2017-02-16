36dc712d3f4d2534d24cc2187ec56ada4cd6fa73
C Powers <cjpowers3@gmail.com>
2017-02-16 03:10:52 -0800

    playback functionality

--- js/index.js
+++ js/index.js
@@ -11,7 +11,29 @@ function keyPress(e) {
     if (e.propertyName !== 'transform') return;
     this.classList.remove('playing')
   }
+
   const keys = document.querySelectorAll('.key-js')
   keys.forEach(k => k.addEventListener('transitionend', removeTransition))
+
+  keyListener.push(e.keyCode)
 }
+
+function playback(clip){
+  console.log('wat')
+  console.log(clip)
+  setTimeout(
+    clip.forEach(key => {
+      console.log(key)
+      const audio = document.querySelector(`audio[data-key='${key}']`)
+      audio.currentTime = 0
+      audio.play()
+    }),
+    500
+  )
+}
+
+var keyListener = []
 window.addEventListener('keydown', keyPress)
+document.getElementById("full-playback").addEventListener("click", function () {
+  playback(keyListener)
+})
